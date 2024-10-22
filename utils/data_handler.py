94acd6af906d57e9a975ffc6cd80b59e440418a4
AhmedElmahhdy <ahmedalaa151002@gmail.com>
2024-10-22 23:09:23 +0300

    solve some issues

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 def load_data(file_path):
     """Load data from a file."""
-    if file_path.type.endswith('.csv'):
+    print(file_path)
+    if file_path.name.endswith('.csv'):
         return pd.read_csv(file_path)
-    elif file_path.type.endswith('.xlsx'):
+    elif file_path.name.endswith('.xlsx'):
         return pd.read_excel(file_path)
     else:
         raise ValueError("Unsupported file format")
